014314ed9162ec2b2c8c3fb6f30257936305038f
terminal29 <20899505+terminal29@users.noreply.github.com>
2020-04-18 17:32:42 +1000

    Hooked up player controls

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -28,24 +28,96 @@ public class PlayerController : MonoBehaviour
     private State state;
     private Direction direction;
 
+    public float moveSpeed = 1.0f;
+    public float jumpPower = 1.0f;
+    public LayerMask groundCollisionMask;
+
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D body;
+    private CapsuleCollider2D capCollider;
     Coroutine animator;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
+        capCollider = GetComponent<CapsuleCollider2D>();
         SetState(State.Idle, Direction.Left);
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isOnGround = IsOnGround();
 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            Jump();
+        }
+
+        Move();
+
+        if (isOnGround)
+        {
+            if (body.velocity.magnitude < 0.1f)
+            {
+                SetState(State.Idle, direction);
+            }
+            else
+            {
+                SetState(State.Walk, direction);
+            }
+        }
+        else
+        {
+            if (body.velocity.y > 0)
+            {
+                SetState(State.Jump, direction);
+            }
+            else
+            {
+                SetState(State.Fall, direction);
+            }
+        }
+
+        if (body.velocity.magnitude > 0.1)
+        {
+            if (body.velocity.x < 0)
+            {
+                SetState(state, Direction.Left);
+            }
+            else
+            {
+                SetState(state, Direction.Right);
+            }
+        }
+    }
+
+    private bool IsOnGround()
+    {
+        RaycastHit2D ray = Physics2D.BoxCast(capCollider.bounds.center, capCollider.bounds.size, 0f, Vector2.down, 0.1f, groundCollisionMask);
+        return ray.collider != null;
+    }
+
+    private void Jump()
+    {
+        Vector2 velocity = body.velocity;
+        velocity.y = jumpPower;
+        body.velocity = velocity;
+    }
+
+    private void Move()
+    {
+        Vector2 velocity = body.velocity;
+        velocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+        body.velocity = velocity;
     }
 
     private void SetState(State state, Direction direction)
     {
+        if (this.state == state && this.direction == direction)
+            return;
         if (animator != null)
             StopCoroutine(animator);
         this.state = state;
